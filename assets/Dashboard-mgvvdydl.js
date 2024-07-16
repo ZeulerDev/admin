af8afb2dfc9aaa95854460483a5cec3d40e42a8e
chamod2000 <maduwanthachamod01@gmail.com>
2024-07-16 11:04:07 +0530

    Updates

---
+++ assets/Dashboard-mgvvdydl.js
@@ -0,0 +1,1 @@
+import"./index-B0LDsaUc.js";import"./index-BBw1DbmA.js";const a=()=>{};export{a as default};
---
+++ assets/FlatRateVatRegistrationPDF-B0nXiH_W.js
@@ -0,0 +1,1 @@
+import"./index-B0LDsaUc.js";import{S as e}from"./react-pdf.browser-ChzlIrVt.js";e.create({page:{flexDirection:"row",backgroundColor:"#ffffff"},section:{margin:0,padding:20,flexGrow:1},font:{textAlign:"right"},image:{width:"10%",height:"10%"}});
