9111ec2b3390d23e1146d07b5b1250466714a90a
chamod2000 <maduwanthachamod01@gmail.com>
2024-07-16 13:25:27 +0530

    add changers

--- src/views/pages/picker/Payout.js
+++ src/views/pages/picker/Payout.js
@@ -34,6 +34,7 @@ import {
 // import FlatRateVatRegistrationPDF from '../../../components/FlatRateVatRegistrationPDF'
 // import {OccasionalEmployeePDF} from '../../../components/OccasionalEmployeePDF'
 const FlatRateVatRegistrationPDF = React.lazy(()=>import('../../../components/FlatRateVatRegistrationPDF'))
+const OccasionalEmployeePDF = React.lazy(()=>import('../../../components/OccasionalEmployeePDF'))
 
 import { BASE_URL } from '../../../context/config'
 
