f0c5b3044672bcb833bccbef3dafbc884184b8a3
chamod2000 <maduwanthachamod01@gmail.com>
2024-09-27 12:57:19 +0530

    add latest changers

--- src/views/pages/Market/Group/MarketDistances.js
+++ src/views/pages/Market/Group/MarketDistances.js
@@ -22,6 +22,7 @@ import {
   import { useAppContext } from '../../../../context/AppContext'
   import axios from 'axios'
 import { BASE_URL } from '../../../../context/config'
+import { SET_ALERT } from '../../../../context/context_reducer'
 
 const MarketDistance = ()=>{
 
@@ -29,8 +30,13 @@ const MarketDistance = ()=>{
   const [{ user, token }, dispatch] = useAppContext()
   const [marketLocation, setMarketLocation] = useState([])
   const [loading, setLoading] = useState(false)
+  const [loadingRoutes, setLoadingRoutes] = useState(false)
 
   useEffect(() => {
+    loadData(id)
+  },[user])
+
+  const loadData = (id)=>{
     if(id){
       if (user && token) {
         setLoading(true)
@@ -60,15 +66,80 @@ const MarketDistance = ()=>{
       }
 
     }else{
-      alert('Market not found')
+      dispatch({
+        type : SET_ALERT,
+        payload : {
+          status : true,
+          title : 'Id Not found',
+          color : 'danger',
+        }
+      })
     }
-  },[user])
+  }
+
+  const fetchRoutes = (id)=>{
+    if(user && token){
+      setLoadingRoutes(true)
+      axios.get(BASE_URL+`market/group/fetch/routes/${id}`,{
+        headers : {
+          Authorization : `Bearer ${token}`
+        }
+      })
+      .then((res)=>{
+        if(res.status === 200){
+          setLoadingRoutes(false)
+          console.log('load data')
+          dispatch({
+            type : SET_ALERT,
+            payload : {
+              status : true,
+              title : 'Routes fetched',
+              message : 'Routes fetched successfully',
+              color : 'success'
+            }
+          })
+          loadData(id)
+
+          
+        }else if(res.status === 500){
+          dispatch({
+            type : SET_ALERT,
+            payload : {
+              status : true,
+              title : 'Routes fetching error',
+              message : res.data.message,
+              color : 'danger'
+            }
+          })
+        }
+      })
+
+
+    }else{
+      dispatch({
+        type : SET_ALERT,
+        payload : {
+          status : true,
+          title : 'User not found',
+          message : 'Please login to continue'
+        }
+      })
+    }
+  }
  
 
   
     return(
         <CContainer>
-        <CNavbar className="bg-body-tertiary">
+            <CButton onClick={()=>{fetchRoutes(id)}} style={{ marginLeft: '82%', width: '17%', backgroundColor: '#ff4d4d', color: 'white' }}>
+                   {loadingRoutes ? <CSpinner color="info" size="sm"/> : 
+                   <>  Fetch Routes</>
+
+                   }
+                   
+                 
+            </CButton>
+        <CNavbar style={{ marginTop: '1%' }} className="bg-body-tertiary">
        
         </CNavbar>
   
@@ -112,3 +183,406 @@ const MarketDistance = ()=>{
 }
 
 export default MarketDistance
+
+
+// Total Routes: [
+//   {
+//     origin: {
+//       lat: 40.84389883325273,
+//       lng: 14.233272543997437,
+//       address: 'Via Archimede, 8, Milano',
+//       _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//     },
+//     destination: {
+//       lat: 45.526714415675954,
+//       lng: 9.17378568983078,
+//       address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//     },
+//     totalDuration: 27845,
+//     totalDistance: 791262,
+//     route: [ [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 40.84389883325273,
+//       lng: 14.233272543997437,
+//       address: 'Via Archimede, 8, Milano',
+//       _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//     },
+//     destination: {
+//       lat: 40.8723052759449,
+//       lng: 14.235817805589758,
+//       address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//     },
+//     totalDuration: 753,
+//     totalDistance: 4700,
+//     route: [ [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 45.526714415675954,
+//       lng: 9.17378568983078,
+//       address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//     },
+//     destination: {
+//       lat: 40.84389883325273,
+//       lng: 14.233272543997437,
+//       address: 'Via Archimede, 8, Milano',
+//       _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//     },
+//     totalDuration: 27799,
+//     totalDistance: 791776,
+//     route: [ [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 45.526714415675954,
+//       lng: 9.17378568983078,
+//       address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//     },
+//     destination: {
+//       lat: 40.8723052759449,
+//       lng: 14.235817805589758,
+//       address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//     },
+//     totalDuration: 27496,
+//     totalDistance: 789997,
+//     route: [ [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 40.8723052759449,
+//       lng: 14.235817805589758,
+//       address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//     },
+//     destination: {
+//       lat: 40.84389883325273,
+//       lng: 14.233272543997437,
+//       address: 'Via Archimede, 8, Milano',
+//       _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//     },
+//     totalDuration: 801,
+//     totalDistance: 4661,
+//     route: [ [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 40.8723052759449,
+//       lng: 14.235817805589758,
+//       address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//     },
+//     destination: {
+//       lat: 45.526714415675954,
+//       lng: 9.17378568983078,
+//       address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//     },
+//     totalDuration: 27921,
+//     totalDistance: 791609,
+//     route: [ [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 40.84389883325273,
+//       lng: 14.233272543997437,
+//       address: 'Via Archimede, 8, Milano',
+//       _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//     },
+//     waypoints: [ [Object] ],
+//     destination: {
+//       lat: 40.8723052759449,
+//       lng: 14.235817805589758,
+//       address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//     },
+//     totalDuration: 55341,
+//     totalDistance: 1581259,
+//     route: [ [Object], [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 40.84389883325273,
+//       lng: 14.233272543997437,
+//       address: 'Via Archimede, 8, Milano',
+//       _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//     },
+//     waypoints: [ [Object] ],
+//     destination: {
+//       lat: 45.526714415675954,
+//       lng: 9.17378568983078,
+//       address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//     },
+//     totalDuration: 28674,
+//     totalDistance: 796309,
+//     route: [ [Object], [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 45.526714415675954,
+//       lng: 9.17378568983078,
+//       address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//     },
+//     waypoints: [ [Object] ],
+//     destination: {
+//       lat: 40.8723052759449,
+//       lng: 14.235817805589758,
+//       address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//     },
+//     totalDuration: 28552,
+//     totalDistance: 796476,
+//     route: [ [Object], [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 45.526714415675954,
+//       lng: 9.17378568983078,
+//       address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//     },
+//     waypoints: [ [Object] ],
+//     destination: {
+//       lat: 40.84389883325273,
+//       lng: 14.233272543997437,
+//       address: 'Via Archimede, 8, Milano',
+//       _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//     },
+//     totalDuration: 28297,
+//     totalDistance: 794658,
+//     route: [ [Object], [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 40.8723052759449,
+//       lng: 14.235817805589758,
+//       address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//     },
+//     waypoints: [ [Object] ],
+//     destination: {
+//       lat: 45.526714415675954,
+//       lng: 9.17378568983078,
+//       address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//     },
+//     totalDuration: 28646,
+//     totalDistance: 795923,
+//     route: [ [Object], [Object] ]
+//   },
+//   {
+//     origin: {
+//       lat: 40.8723052759449,
+//       lng: 14.235817805589758,
+//       address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//       _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//     },
+//     waypoints: [ [Object] ],
+//     destination: {
+//       lat: 40.84389883325273,
+//       lng: 14.233272543997437,
+//       address: 'Via Archimede, 8, Milano',
+//       _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//     },
+//     totalDuration: 55720,
+//     totalDistance: 1583385,
+//     route: [ [Object], [Object] ]
+//   }
+// ]
+
+
+
+
+// origin: {
+//   lat: 40.84389883325273,
+//   lng: 14.233272543997437,
+//   address: 'Via Archimede, 8, Milano',
+//   _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+// } destination: {
+//   lat: 45.526714415675954,
+//   lng: 9.17378568983078,
+//   address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+// } waypoints: []
+// origin: {
+//   lat: 40.84389883325273,
+//   lng: 14.233272543997437,
+//   address: 'Via Archimede, 8, Milano',
+//   _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+// } destination: {
+//   lat: 40.8723052759449,
+//   lng: 14.235817805589758,
+//   address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+// } waypoints: []
+// origin: {
+//   lat: 45.526714415675954,
+//   lng: 9.17378568983078,
+//   address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+// } destination: {
+//   lat: 40.84389883325273,
+//   lng: 14.233272543997437,
+//   address: 'Via Archimede, 8, Milano',
+//   _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+// } waypoints: []
+// origin: {
+//   lat: 45.526714415675954,
+//   lng: 9.17378568983078,
+//   address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+// } destination: {
+//   lat: 40.8723052759449,
+//   lng: 14.235817805589758,
+//   address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+// } waypoints: []
+// origin: {
+//   lat: 40.8723052759449,
+//   lng: 14.235817805589758,
+//   address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+// } destination: {
+//   lat: 40.84389883325273,
+//   lng: 14.233272543997437,
+//   address: 'Via Archimede, 8, Milano',
+//   _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+// } waypoints: []
+// origin: {
+//   lat: 40.8723052759449,
+//   lng: 14.235817805589758,
+//   address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+// } destination: {
+//   lat: 45.526714415675954,
+//   lng: 9.17378568983078,
+//   address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+// } waypoints: []
+// others: length 6
+// origin: {
+//   lat: 40.84389883325273,
+//   lng: 14.233272543997437,
+//   address: 'Via Archimede, 8, Milano',
+//   _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+// } destination: {
+//   lat: 40.8723052759449,
+//   lng: 14.235817805589758,
+//   address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+// } waypoints: [
+//   {
+//     lat: 45.526714415675954,
+//     lng: 9.17378568983078,
+//     address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//     _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//   }
+// ]
+// duration: 27942 distance: 794080
+// duration: 27966 distance: 792517
+// origin: {
+//   lat: 40.84389883325273,
+//   lng: 14.233272543997437,
+//   address: 'Via Archimede, 8, Milano',
+//   _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+// } destination: {
+//   lat: 45.526714415675954,
+//   lng: 9.17378568983078,
+//   address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+// } waypoints: [
+//   {
+//     lat: 40.8723052759449,
+//     lng: 14.235817805589758,
+//     address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//     _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//   }
+// ]
+// duration: 753 distance: 4700
+// duration: 28018 distance: 794427
+// origin: {
+//   lat: 45.526714415675954,
+//   lng: 9.17378568983078,
+//   address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+// } destination: {
+//   lat: 40.8723052759449,
+//   lng: 14.235817805589758,
+//   address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+// } waypoints: [
+//   {
+//     lat: 40.84389883325273,
+//     lng: 14.233272543997437,
+//     address: 'Via Archimede, 8, Milano',
+//     _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//   }
+// ]
+// duration: 28269 distance: 794296
+// duration: 753 distance: 4700
+// origin: {
+//   lat: 45.526714415675954,
+//   lng: 9.17378568983078,
+//   address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+// } destination: {
+//   lat: 40.84389883325273,
+//   lng: 14.233272543997437,
+//   address: 'Via Archimede, 8, Milano',
+//   _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+// } waypoints: [
+//   {
+//     lat: 40.8723052759449,
+//     lng: 14.235817805589758,
+//     address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//     _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+//   }
+// ]
+// duration: 27966 distance: 792517
+// duration: 801 distance: 4661
+// origin: {
+//   lat: 40.8723052759449,
+//   lng: 14.235817805589758,
+//   address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+// } destination: {
+//   lat: 45.526714415675954,
+//   lng: 9.17378568983078,
+//   address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+// } waypoints: [
+//   {
+//     lat: 40.84389883325273,
+//     lng: 14.233272543997437,
+//     address: 'Via Archimede, 8, Milano',
+//     _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+//   }
+// ]
+// duration: 801 distance: 4661
+// duration: 27942 distance: 794080
+// origin: {
+//   lat: 40.8723052759449,
+//   lng: 14.235817805589758,
+//   address: 'Via Giuseppe Tartini, 4/A, 20158 Milano MI, Italy',
+//   _id: new ObjectId('6618c8b78c9c48a7fa1c880a')
+// } destination: {
+//   lat: 40.84389883325273,
+//   lng: 14.233272543997437,
+//   address: 'Via Archimede, 8, Milano',
+//   _id: new ObjectId('62b29f4ebfc13d66d0251fc1')
+// } waypoints: [
+//   {
+//     lat: 45.526714415675954,
+//     lng: 9.17378568983078,
+//     address: 'Via Giuseppe Tartini, 3/A, 20158 Milano MI, Italy',
+//     _id: new ObjectId('6617c771fdd4c9cde96b5ac3')
+//   }
+// ]
